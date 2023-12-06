512a0c6ff26b68d711a5c39e080693c222830bba
andradajus <andradajustinmarcus@gmail.com>
2023-12-06 22:04:12 +0800

    Tweak UI

--- app/controllers/reminder_controller.rb
+++ app/controllers/reminder_controller.rb
@@ -1,5 +1,7 @@
 class ReminderController < ApplicationController
   def index
+    @reminders_due_today = Reminder.due_today
+    @reminders_due_tomorrow = Reminder.due_tomorrow
     @reminders_within_week = Reminder.due_within_week
     @reminders_within_30_days = Reminder.due_within_30_days
   end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,7 +1,8 @@
 class Reminder < ApplicationRecord
   # belongs_to :user
-  class Reminder < ApplicationRecord
-    scope :due_within_week, -> { where('date <= ?', 1.week.from_now) }
-    scope :due_within_30_days, -> { where('date <= ?', 30.days.from_now) }
+  scope :due_today, -> { where(date: Date.today) }
+  scope :due_tomorrow, -> { where(date: Date.tomorrow) }
+  scope :due_within_week, -> { where('date <= ?', 1.week.from_now) }
+  scope :due_within_30_days, -> { where('date <= ?', 30.days.from_now)
   end
 end
